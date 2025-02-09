11bb59e77d68aade1318325b71e5c591c98cf980
ridzky rahim <ridzkyrhm37@gmail.com>
2025-02-10 04:41:13 +0700

    memasang template UI

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/catalog', function () {
+    return view('catalog');
+});
+
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
